1351a2a66951317bc2e6fc3e84bdf83c74e5605d
Benedito Cardoso <bneto@pucrs.br>
2023-05-22 22:23:57 -0300

    added equals and hashCode method for Card.

--- src/main/java/com/altbank/data/Card.java
+++ src/main/java/com/altbank/data/Card.java
@@ -1,9 +1,11 @@
 package com.altbank.data;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 public class Card {
 
