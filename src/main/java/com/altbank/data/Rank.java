ffb5cddc165d8e22d998533a56f7cc3bfdd61046
Benedito Cardoso <bneto@pucrs.br>
2023-05-24 10:17:50 -0300

    added description to ranks and suits.

--- src/main/java/com/altbank/data/Rank.java
+++ src/main/java/com/altbank/data/Rank.java
@@ -3,24 +3,28 @@ package com.altbank.data;
 import lombok.Getter;
 
 public enum Rank {
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(11),
-    QUEEN(11),
-    KING(11),
-    ACE(10);
+
+    TWO(2, "Dois"),
+    THREE(3, "Três"),
+    FOUR(4, "Quatro"),
+    FIVE(5, "Cinco"),
+    SIX(6, "Seis"),
+    SEVEN(7, "Sete"),
+    EIGHT(8, "Oito"),
+    NINE(9, "Nove"),
+    TEN(10, "Dez"),
+    JACK(11, "Valete"),
+    QUEEN(11, "Dama"),
+    KING(11, "Rei"),
+    ACE(10, "As");
 
     @Getter
     private final int points;
+    @Getter
+    private final String desc;
 
-    Rank(int points) {
+    Rank(int points, String desc) {
         this.points = points;
+        this.desc = desc;
     }
 }
--- src/main/java/com/altbank/data/Suit.java
+++ src/main/java/com/altbank/data/Suit.java
@@ -1,5 +1,17 @@
 package com.altbank.data;
 
+import lombok.Getter;
+
 public enum Suit {
-    CLUBS, DIAMONDS, HEARTS, SPADES
+    CLUBS("Paus"),
+    DIAMONDS("Ouros"),
+    HEARTS("Copas"),
+    SPADES("Espadas");
+
+    @Getter
+    private final String desc;
+
+    Suit(String desc) {
+        this.desc = desc;
+    }
 }
